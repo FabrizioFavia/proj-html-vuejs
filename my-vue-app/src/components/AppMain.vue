385fe38e491c677f618a373e1884f40291a34c7b
FabrizioFavia <personal.favia@gmail.com>
2023-06-05 18:18:00 +0200

    feat: added keyframe to button

--- my-vue-app/src/components/AppMain.vue
+++ my-vue-app/src/components/AppMain.vue
@@ -18,7 +18,7 @@ export default {
         AppFilterSection,
         AppLatestNews,
         AppSubscribe,
-    }
+    },
 }
 </script>
 
--- my-vue-app/src/components/AppTopArrow.vue
+++ my-vue-app/src/components/AppTopArrow.vue
@@ -2,24 +2,101 @@
 
 export default {
     name: "AppTopArrow",
+    methods: {
+        onScroll(event) {
+            const button = document.getElementById("topButton");
+            if (button) {
+                if (document.documentElement.scrollTop > 150) {
+                    button.classList.add("goToTop-zoom")
+                } else {
+                    button.classList.remove("goToTop-zoom")
+                }
+            }
+
+        }
+    },
+    created() {
+        window.addEventListener("scroll", this.onScroll);
+    },
+    unmounted() {
+        window.removeEventListener("scroll", this.onScroll);
+    }
 
 }
 
 </script>
 
 <template>
-    <a href="#"><button><i class="fa-solid fa-chevron-up"></i></button></a>
+    <a class="goToTop" id="topButton" href="#">
+        <button class="w-100 h-100">
+            <i class="fa-solid fa-chevron-up"></i>
+        </button>
+    </a>
 </template>
 
 
-<style scoped>
-button {
-    width: 40px;
-    height: 40px;
-    position: fixed;
-    bottom: 150px;
-    right: 40px;
-    z-index: 10;
-    font-size: 11px;
+<style lang="scss" scoped>
+@use "../main.scss" as *;
+
+.goToTop {
+    opacity: 80%;
+
+    position: fixed !important;
+
+    bottom: 70px !important;
+
+    right: 2vw !important;
+
+    width: 0px;
+
+    height: 0px;
+
+    border-width: 0px;
+
+    background-color: $primaryColor;
+
+    transition: width 0.5s, height 0.5s;
+
+}
+
+@-webkit-keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
+}
+
+@keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
+}
+
+.goToTop-zoom button {
+    display: unset !important;
+    fill: white !important;
+    animation: fadeIn 1s;
+    -webkit-animation: fadeIn 1s;
+    stroke: white;
+}
+
+.goToTop button {
+    display: none;
+    fill: transparent;
+    animation: fadeIn 1s;
+    -webkit-animation: fadeIn 1s;
+}
+
+.goToTop-zoom {
+    width: 32px;
+    height: 32px;
+    z-index: 100;
 }
 </style>
